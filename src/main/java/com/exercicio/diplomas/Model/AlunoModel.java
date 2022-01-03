26f61778c363a5d9ee2831ee011068a6bd3fd99a
anfranco <ana.franco@mercadolivre.com>
2022-01-03 20:02:22 -0300

    adding validation

--- src/main/java/com/exercicio/diplomas/Model/AlunoModel.java
+++ src/main/java/com/exercicio/diplomas/Model/AlunoModel.java
@@ -5,13 +5,20 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+
 public class AlunoModel {
+
+    @NotNull
+    @NotEmpty(message = "A disciplina é obrigatório")
     String nome;
     List<DisciplinaModel> disciplinas;
+
 }
--- src/main/java/com/exercicio/diplomas/Model/DisciplinaModel.java
+++ src/main/java/com/exercicio/diplomas/Model/DisciplinaModel.java
@@ -5,13 +5,22 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class DisciplinaModel {
 
+    @NotNull
+    @NotEmpty(message = "A disciplina é obrigatório")
     String nome;
+
+    @NotEmpty(message = "A nota é obrigatório")
+    @Pattern(regexp = "[0-9]", flags = Pattern.Flag.UNICODE_CASE)
     double nota;
 
 }
